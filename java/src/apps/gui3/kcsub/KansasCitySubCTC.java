e770386d44538635e20d8a26c13739f0ec8151a6
Kelly Loyd <kelly.loyd@internode.on.net>
2015-12-24 08:20:21 +1100

    Starting Kansas City Subdivision CTC project

---
+++ java/src/apps/gui3/kcsub/KansasCitySubCTC.java
@@ -0,0 +1,27 @@
+package apps.gui3.kcsub;
+
+import java.util.ResourceBundle;
+
+public class KansasCitySubCTC extends apps.gui3.Apps3 {
+
+    private static String menuFile = null;
+    private static String toolbarFile = null;
+    private static final String applicationName = "KansasCitySubCTC";
+    
+    public KansasCitySubCTC(String[] args) {
+        super(applicationName, "KCSubCTC.xml", args);
+    }
+    
+    @Override
+    protected void createMainFrame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected ResourceBundle getActionModelResourceBundle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
